c440a7c82b4e59aa8a043f8ac8c0213caa422e71
Jeffrey <eggert.jeff@gmail.com>
2020-03-20 08:30:37 -0400

    jenkins added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
@@ -7,11 +6,11 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
+import Services from './Components/Services';
 import Portfolio from './Components/Portfolio';
-
 import * as Icons from "@fortawesome/free-brands-svg-icons";
 import { library } from '@fortawesome/fontawesome-svg-core';
+
 const iconList = Object
   .keys(Icons)
   .filter(key => key !== "fas" && key !== "prefix" )
@@ -28,9 +27,6 @@ class App extends Component {
       foo: 'bar',
       resumeData: {}
     };
-
-   // ReactGA.initialize('UA-110570651-1');
-    //ReactGA.pageview(window.location.pathname);
 
   }
 
@@ -60,7 +56,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Services data={this.state.resumeData.services}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
